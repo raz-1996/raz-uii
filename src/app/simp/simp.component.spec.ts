b69414b93cc0bdfaf2bfdfa2bc0419b725e42d56
Razmik <hovhannisyan.razmik.1996@gmail.com>
2022-10-16 21:49:11 +0400

    init

---
+++ src/app/simp/simp.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SimpComponent } from './simp.component';
+
+describe('SimpComponent', () => {
+  let component: SimpComponent;
+  let fixture: ComponentFixture<SimpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SimpComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SimpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
